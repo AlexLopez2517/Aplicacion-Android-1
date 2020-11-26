e0ca92812205622c2a08fd8e80136e1283c454ef
AlexLopez2517 <alex2761086444@gmail.com>
2020-11-26 00:44:03 -0600

    Falto documento del jsonArr y no hay nada en cloud firestore

--- app/src/main/java/com/platzi/conf/MainActivity.kt
+++ app/src/main/java/com/platzi/conf/MainActivity.kt
@@ -2,11 +2,49 @@ package com.platzi.conf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Conference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.platzi.conf.R
+import com.platzi.conf.model.Conferense
+import com.platzi.conf.model.Speaker
+import org.json.JSONObject
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val firebaseFirestore = FirebaseFirestore.getInstance()
+
+        /*for(i in 0 until jsonArr.length()){
+            val aux = jsonArr.get(i) as JSONObject
+            var speaker = Speaker()
+            speaker.name = aux.getString("name")
+            speaker.jobtitle = aux.getString("jobtitle")
+            speaker.workplace = aux.getString("workplace")
+            speaker.biography = aux.getString("biography")
+            speaker.twitter = aux.getString("twitter")
+            speaker.image = aux.getString("image")
+            speaker.category = aux.getInt("category")
+
+            firebaseFirestore.collection("speakers").document().set(speaker)
+        }
+
+        for(i in 0 until jsonArr2.length()){
+            val aux = jsonArr2.get(i) as JSONObject
+            var conference = Conferense()
+            conference.title = aux.getString("title")
+            conference.description = aux.getString("description")
+            conference.tag = aux.getString("tag")
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = aux.getLong("datetime") * 1000
+            conference.datetime = cal.time
+            conference.speaker = aux.getLong("speaker")
+
+            firebaseFirestore.collection("conferences").document().set(conference)
+        }
+
+    }*/
     }
 }
