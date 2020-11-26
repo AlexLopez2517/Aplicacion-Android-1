3e1f6f303837b80b8ca0a03645fefeacb7b1100a
AlexLopez2517 <alex2761086444@gmail.com>
2020-11-25 23:53:09 -0600

    Conectando el proyecto con Firebase

---
+++ app/src/main/java/com/platzi/conf/model/Conferense.kt
@@ -0,0 +1,10 @@
+package com.platzi.conf.model
+
+import java.util.*
+
+class Conferense {
+    lateinit var title: String
+    lateinit var description: String
+    lateinit var tag: String
+    lateinit var dateTime: Date
+}
